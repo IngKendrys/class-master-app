0d11b1bf9fc4eaa93a1d76a4fefdce350cbe2358
Jesus Bermudez <jesmanuelber@gmail.com>
2023-11-23 18:29:03 -0500

    .

--- src/App/Access/Access.jsx
+++ src/App/Access/Access.jsx
@@ -16,9 +16,9 @@ export default function Access({Form = "Login"}) {
     <section className="Access">
       {currentForm === "Login" ? <Login onFormSwitch={toggleForm}/> : <Signin onFormSwitch={toggleForm}/>}
       <div className="access-box imagen-svg">
-        <picture className="imagen-svg__img">
-          <source srcSet={ilustration1} type="image/webp" />
-          <img src={ilustration} ></img>
+        <picture>
+          <source srcSet={ilustration1} type="image/webp" className="imagen-svg__img" />
+          <img src={ilustration} className="imagen-svg__img" ></img>
         </picture>
       </div>
     </section>
